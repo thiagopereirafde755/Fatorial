d13499beb08816ffa1cc383dc16eaf3200fcd3b1
Thiago Pereira <167644192+thiagopereirafde755@users.noreply.github.com>
2024-12-29 16:39:13 -0300

    Atualizar o Fatorial.java

--- com.mycompany_Fatorial_jar_1.0-SNAPSHOT/src/main/java/com/mycompany/fatorial/Fatorial.java
+++ com.mycompany_Fatorial_jar_1.0-SNAPSHOT/src/main/java/com/mycompany/fatorial/Fatorial.java
@@ -4,20 +4,20 @@ import javax.swing.JOptionPane;
 
 public class Fatorial {
     public static void main(String[] args) {
-        // Get the input from the user
+        
         double numero = Integer.parseInt(JOptionPane.showInputDialog("Digite um número para calcular o fatorial:"));
         
-        // Initialize variables
+    
         long fatorial = 1;
         int contador = (int) numero;
         
-        // Calculate the factorial
+       
         while (contador > 1) {
             fatorial *= contador;
             contador--;
         }
         
-        // Display the result
+        
         JOptionPane.showMessageDialog(null, "O fatorial de " + numero + " é: " + fatorial);
     }
 }
